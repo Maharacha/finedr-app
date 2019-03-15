72a41f4c7ca8ea73b35579949de2712d692f94b2
Joakim Nyman <joakim.nyman86@gmail.com>
2019-03-15 11:30:52 +0100

    updated server address

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -5,10 +5,10 @@ import { HTTP } from '@ionic-native/http/ngx';
   providedIn: 'root'
 })
 export class HttpService {
-    //public serverAddress: string = "http://localhost:8080";
-    public serverAddress: string = "http://192.168.1.84:8080/onsite/api";
-    //public serverAddress: string = "http://172.18.17.104:8080";
-    //public serverAddress: string = "http://joakim-nyman.se:8080";
+    //public serverAddress: string = "http://localhost:8080/onsite/api";
+    //public serverAddress: string = "http://192.168.1.84:8080/onsite/api";
+    //public serverAddress: string = "http://172.18.17.104:8080/onsite/api";
+    public serverAddress: string = "http://joakim-nyman.se:8080/onsite/api";
 
     constructor(
 	private http: HTTP
