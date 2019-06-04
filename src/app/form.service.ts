a267bd8cbe01d968cc8544fa0e80221118254b3e
Joakim Nyman <joakim.nyman86@gmail.com>
2019-06-04 23:42:37 +0200

    added current location to finetip

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HTTP } from '@ionic-native/http/ngx';
 import { LoginService } from './login.service';
 import { HttpService } from './http.service';
+import { Location } from './map/location';
 
 @Injectable({
   providedIn: 'root'
@@ -9,33 +10,38 @@ import { HttpService } from './http.service';
 export class FormService {
 
     public base64Image: string;
-
+	public coordinates: Location;
     public fineDetails = {
-	licensePlate: "",
-	reason: ""
+		licensePlate: "",
+		reason: ""
     };
     
     constructor(
         private http: HTTP,
-	private loginService: LoginService,
-	private httpService: HttpService
+		private loginService: LoginService,
+		private httpService: HttpService
     ) { }
 
     public send() {
-	let username = this.loginService.username;
-	let token = this.loginService.token;
-	let serverAddress = this.httpService.serverAddress;
+		let username = this.loginService.username;
+		let token = this.loginService.token;
+		let coordinates = {
+			lat: this.coordinates.latitude,
+			lng: this.coordinates.longitude
+		}
+		let serverAddress = this.httpService.serverAddress;
     	this.http.post(this.httpService.serverAddress + '/finetip/', {
     	    'image': this.base64Image,
     	    'license_plate': this.fineDetails.licensePlate,
-	    'reason': this.fineDetails.reason,
-	    'parking_lot_name': 'Ringgatan 18-22',
-	    'created_by_name': username
-    	}, {
-	    'Authorization': 'Token ' + token
-	}).then(request => {
-	    console.log(request);
-    	}).catch(error => {
+			'reason': this.fineDetails.reason,
+			'parking_lot_name': 'Ringgatan 18-22',
+			'created_by_name': username,
+			'coordinates': JSON.stringify(coordinates)
+			}, {
+			'Authorization': 'Token ' + token
+		}).then(request => {
+	    	console.log(request);
+		}).catch(error => {
     	    console.log(error.status);
     	    console.log(error.error); // error message as string
     	    console.log(error.headers);
--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -50,208 +50,206 @@ export class MapPage implements OnInit {
     positionWatcherObj;
     
     constructor(
-	public loadingCtrl: LoadingController,
-	public toastCtrl: ToastController,
-	private platform: Platform,
-	private router: Router,
-	private http: HTTP,
-	private geolocation: Geolocation,
-	private loginService: LoginService,
-	private mapService: MapService,
-	private formService: FormService) {
-	this.setBrowserApiKeys();
-	this.currentLocation = new Location();
-    }
+		public loadingCtrl: LoadingController,
+		public toastCtrl: ToastController,
+		private platform: Platform,
+		private router: Router,
+		private http: HTTP,
+		private geolocation: Geolocation,
+		private loginService: LoginService,
+		private mapService: MapService,
+		private formService: FormService) {
+			this.setBrowserApiKeys();
+			this.currentLocation = new Location();
+    	}
 
     async ngOnInit() {
-
-	// Since ngOnInit() is executed before `deviceready` event,
-	// you have to wait the event.
-	await this.platform.ready();
-	await this.loadMap();
+		// Since ngOnInit() is executed before `deviceready` event,
+		// you have to wait the event.
+		await this.platform.ready();
+		await this.loadMap();
     }
 
     loadMap() {
-	let mapOptions: GoogleMapOptions = {
-	    'controls': {
-		'compass': true,
-		'myLocation': true,
-		'myLocationButton': true,
-		'zoom': true
-	    }
-	};
-	
-	this.map = GoogleMaps.create('map_canvas', mapOptions);
-	this.map.one(GoogleMapsEvent.MAP_READY).then(this.onMapReady.bind(this));
+		let mapOptions: GoogleMapOptions = {
+			'controls': {
+			'compass': true,
+			'myLocation': true,
+			'myLocationButton': true,
+			'zoom': true
+			}
+		};	
+		this.map = GoogleMaps.create('map_canvas', mapOptions);
+		this.map.one(GoogleMapsEvent.MAP_READY).then(this.onMapReady.bind(this));
     }
 
     setBrowserApiKeys() {
-	Environment.setEnv({
-	    'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDgWtSOMi0yya4xqBelBI2ErAOdsdvxJbQ',
-	    'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDgWtSOMi0yya4xqBelBI2ErAOdsdvxJbQ'
-	});
+		Environment.setEnv({
+			'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDgWtSOMi0yya4xqBelBI2ErAOdsdvxJbQ',
+			'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDgWtSOMi0yya4xqBelBI2ErAOdsdvxJbQ'
+		});
     }
 
     onMapReady() {
-	this.nextPageButtonText = "No parking";
-	this.nextPageButtonColor = "danger";
-	
-	var nextPageButton = document.getElementById('nextPageButton') as HTMLButtonElement;
-	nextPageButton.disabled = true;	
-	this.nextPageButtonDisabled = true;
-	
-	this.getCurrentLocation().then(() => {
-	    this.moveCamera(this.currentLocation.latitude, this.currentLocation.longitude);
-	});
-	
-	this.mapService.getParkingLotsFromApi(this.parseAndAddParkingLots.bind(this));
+		this.nextPageButtonText = "No parking";
+		this.nextPageButtonColor = "danger";
+		
+		var nextPageButton = document.getElementById('nextPageButton') as HTMLButtonElement;
+		nextPageButton.disabled = true;	
+		this.nextPageButtonDisabled = true;
+		
+		this.getCurrentLocation().then(() => {
+			this.moveCamera(this.currentLocation.latitude, this.currentLocation.longitude);
+		});
+		
+		this.mapService.getParkingLotsFromApi(this.parseAndAddParkingLots.bind(this));
 
-	this.watchPosition();
+		this.watchPosition();
     }
 
     checkIfInsidePolygon(location: Location) {
-	let insideAPolygon = false;
-	for (let parkingLot of this.parkingLots) {
-	    let inside = Poly.containsLocation({lat: location.latitude, lng: location.longitude}, parkingLot.points);
-	    this.updateParkingLotState(parkingLot, inside);
-	    if (inside)
-		insideAPolygon = true;
-	}
-	if (insideAPolygon)
-	    this.updateNextPageButton(true);
-	else
-	    this.updateNextPageButton(false);
+		let insideAPolygon = false;
+		for (let parkingLot of this.parkingLots) {
+			let inside = Poly.containsLocation({lat: location.latitude, lng: location.longitude}, parkingLot.points);
+			this.updateParkingLotState(parkingLot, inside);
+			if (inside)
+				insideAPolygon = true;
+		}
+		if (insideAPolygon)
+			this.updateNextPageButton(true);
+		else
+			this.updateNextPageButton(false);
     }
 
     async watchPosition() {
-	return new Promise( async () => {
-	    setTimeout(async () => {
-		this.getCurrentLocation();
-		let location = new Location(this.currentLocation.latitude, this.currentLocation.longitude);
-		this.positionChanged(location);
-		await this.watchPosition();
-	    }, 1000);
-	});
+		return new Promise( async () => {
+			setTimeout(async () => {
+				this.getCurrentLocation();
+				let location = new Location(this.currentLocation.latitude, this.currentLocation.longitude);
+				this.positionChanged(location);
+				await this.watchPosition();
+			}, 1000);
+		});
     }
 
     marker = null;
     addMarker(location: Location) {
-	if (this.marker == null) {
-	    this.map.addMarker({
-		position: {"lat": location.latitude, "lng": location.longitude}
-	    }).then((marker) => {	    
-		this.marker = marker;
-	    });
-	}
-	else {
-	    this.marker.setPosition({
-		lat: location.latitude,
-		lng: location.longitude
-	    });
-	}
+		if (this.marker == null) {
+			this.map.addMarker({
+				position: {"lat": location.latitude, "lng": location.longitude}
+				}).then((marker) => {	    
+				this.marker = marker;
+			});
+		}
+		else {
+			this.marker.setPosition({
+				lat: location.latitude,
+				lng: location.longitude
+			});
+		}
     }
 
     positionChanged(newPosition: Location) {
-	this.checkIfInsidePolygon(newPosition);
+		this.checkIfInsidePolygon(newPosition);
     }
 
     updateParkingLotState(parkingLot: ParkingLot, userIsInside: boolean) {
 	// if (userIsInside != parkingLot.userIsInside) {
 	    parkingLot.userIsInside = userIsInside;
 	    if (userIsInside) {
-		parkingLot.polygon.setFillColor("green");
-		parkingLot.polygon.setStrokeColor("green");
+			parkingLot.polygon.setFillColor("green");
+			parkingLot.polygon.setStrokeColor("green");
 	    }
 	    else {
-		parkingLot.polygon.setFillColor("red");
-		parkingLot.polygon.setStrokeColor("red");	
+			parkingLot.polygon.setFillColor("red");
+			parkingLot.polygon.setStrokeColor("red");	
 	    }
 	// }
     }
 
     updateNextPageButton(enable: boolean) {
-	let nextPageButton = document.getElementById('nextPageButton') as HTMLButtonElement;
-	
-	if (enable) {
-	    this.nextPageButtonText = "Camera";
-	    this.nextPageButtonColor = 'secondary';
-	    
-	    nextPageButton.disabled = false;
-	    this.nextPageButtonDisabled = false;
-	}
-	else {
-	    this.nextPageButtonText = "No parkinglot";
-	    this.nextPageButtonColor = 'danger';
-	    
-	    nextPageButton.disabled = true;
-	    this.nextPageButtonDisabled = true;
-	}
+		let nextPageButton = document.getElementById('nextPageButton') as HTMLButtonElement;
+		
+		if (enable) {
+			this.nextPageButtonText = "Camera";
+			this.nextPageButtonColor = 'secondary';
+			
+			nextPageButton.disabled = false;
+			this.nextPageButtonDisabled = false;
+		}
+		else {
+			this.nextPageButtonText = "No parkinglot";
+			this.nextPageButtonColor = 'danger';
+			
+			nextPageButton.disabled = true;
+			this.nextPageButtonDisabled = true;
+		}
     }
     
     onPolygonClick(data) {
-	this.updateNextPageButton(true);
-	data[1].setFillColor("green");
-	data[1].setStrokeColor("green");
+		this.updateNextPageButton(true);
+		data[1].setFillColor("green");
+		data[1].setStrokeColor("green");
     }
 
     moveCamera(latitude: number, longitude: number) {
-	this.map.animateCamera({
-	    target: {lat: latitude, lng: longitude},
-	    zoom: 15,
-	    bearing: 140,
-	    duration: 500
-	});
+		this.map.animateCamera({
+			target: {lat: latitude, lng: longitude},
+			zoom: 15,
+			bearing: 140,
+			duration: 500
+		});
     }
     
     setMarker(latitude: number, longitude: number) {
-	var marker = this.map.addMarker({
-	    position: {lat: latitude, lng: longitude},
-	    title: "Hello Cordova Google Maps for iOS and Android",
-	    snippet: "This plugin is awesome!"
-	});
+		var marker = this.map.addMarker({
+			position: {lat: latitude, lng: longitude},
+			title: "Hello Cordova Google Maps for iOS and Android",
+			snippet: "This plugin is awesome!"
+		});
     }
     
     parseAndAddParkingLots(jsonObj) {
-	for (let parkingLot of jsonObj) {
-	    let coordinates = JSON.parse(parkingLot.coordinates);	    
-	    this.addParkingLot(new ParkingLot(coordinates));
-	}
+		for (let parkingLot of jsonObj) {
+			let coordinates = JSON.parse(parkingLot.coordinates);	    
+			this.addParkingLot(new ParkingLot(coordinates));
+		}
     }
 
-    addParkingLot(parkingLot: ParkingLot) {
-	
-	let options: PolygonOptions = {
-	    // points: lot.points
-	    points: parkingLot.points,
-	    clickable: true
-	}
-	this.map.addPolygon(options).then((polygon) => {
-	    polygon.on(GoogleMapsEvent.POLYGON_CLICK).subscribe((data) => {
-                this.onPolygonClick(data);
-	    });
-	    parkingLot.polygon = polygon;
-	    this.parkingLots.push(parkingLot);
-	});
+    addParkingLot(parkingLot: ParkingLot) {	
+		let options: PolygonOptions = {
+			// points: lot.points
+			points: parkingLot.points,
+			clickable: true
+		}
+		this.map.addPolygon(options).then((polygon) => {
+			polygon.on(GoogleMapsEvent.POLYGON_CLICK).subscribe((data) => {
+					this.onPolygonClick(data);
+			});
+			parkingLot.polygon = polygon;
+			this.parkingLots.push(parkingLot);
+		});
     }
 
     async getCurrentLocation() {
-	let options: MyLocationOptions = {
-	    enableHighAccuracy: true
-	};
-	await LocationService.getMyLocation(options)
-	    .then((location) => {
-		// alert(["Your current location:\n",
-		//        "latitude:" + location.latLng.lat.toFixed(3),
-		//        "longitude:" + location.latLng.lng.toFixed(3),
-		//        "speed:" + location.speed,
-		//        "time:" + location.time,
-		//        "bearing:" + location.bearing].join("\n"));
-		this.currentLocation.latitude = location.latLng.lat;
-		this.currentLocation.longitude = location.latLng.lng;		
+		let options: MyLocationOptions = {
+	    	enableHighAccuracy: true
+		};
+		await LocationService.getMyLocation(options)
+			.then((location) => {
+			// alert(["Your current location:\n",
+			//        "latitude:" + location.latLng.lat.toFixed(3),
+			//        "longitude:" + location.latLng.lng.toFixed(3),
+			//        "speed:" + location.speed,
+			//        "time:" + location.time,
+			//        "bearing:" + location.bearing].join("\n"));
+			this.currentLocation.latitude = location.latLng.lat;
+			this.currentLocation.longitude = location.latLng.lng;		
 	    });
     }
 
     nextPage() {
-	this.router.navigate(['/camera']);
+		this.formService.coordinates = this.currentLocation;
+		this.router.navigate(['/camera']);
     }
 }
