dc870293f7a10e69d1b50ab4c906d64cb745fa78
Joakim Nyman <joakim.nyman86@gmail.com>
2019-06-10 21:14:15 +0200

    username is restored from storage at validation

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -4,66 +4,73 @@ import { HttpService } from './http.service';
 import { Storage } from '@ionic/storage';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LoginService {
 
-	public username: string;    
-	public token: string;
+    public username: string;    
+    public token: string;
 
-	constructor(
-		private http: HTTP,
-		private httpService: HttpService,
-		private storage: Storage
-	){}
+    constructor(
+	private http: HTTP,
+	private httpService: HttpService,
+	private storage: Storage
+    ){}
 
-	public getToken(username: string, password: string, callbackLoggedIn: Function, callbackNotLoggedIn: Function) {
-		let serverAddress = this.httpService.serverAddress;
+    public getToken(username: string, password: string, callbackLoggedIn: Function, callbackNotLoggedIn: Function) {
+	let serverAddress = this.httpService.serverAddress;
     	this.http.post(this.httpService.serverAddress + '-token-auth/', {
-				'username': username,
-				'password': password
+	    'username': username,
+	    'password': password
     	}, {}).then(request => {
-				let jsonObj = JSON.parse(request.data);
-				let token = jsonObj.token;
-				this.token = token;
-				this.username = username;
-				this.storage.set('username', this.username);
-				this.storage.set('token', this.token);
-	    	callbackLoggedIn();
+	    let jsonObj = JSON.parse(request.data);
+	    let token = jsonObj.token;
+	    this.token = token;
+	    this.username = username;
+	    this.storage.set('username', this.username);
+	    this.storage.set('token', this.token);
+	    callbackLoggedIn();
     	}).catch(error => {
-				console.log(error.status);
-				console.log(error.error);
-				console.log(error.headers);
-	    	callbackNotLoggedIn();
+	    console.log(error.status);
+	    console.log(error.error);
+	    console.log(error.headers);
+	    callbackNotLoggedIn();
     	});
-		}
+    }
 
-	public validateToken(callbackLoggedIn: Function, callbackNotLoggedIn: Function) {
-		console.log("Validates token");
-		this.storage.get('token').then((token) => {
-			console.log(token);
-			this.token = token;
-			let serverAddress = this.httpService.serverAddress;
-			this.http.get(serverAddress + '/getusername/', {}, {
-				Authorization: 'Token ' + token
-			})
-			.then(request => {
-				console.log(request.status);
-				console.log(request.data);
-				console.log(request.headers);
-				callbackLoggedIn();
-			})
-			.catch(error => {
-				console.log(error.status);
-				console.log(error.error);
-				console.log(error.headers);
-				callbackNotLoggedIn();
-			});
-		}).catch(error => {
-			console.log(error);
-			console.log("Could not get token from storage.");
-			callbackNotLoggedIn();
+    public validateToken(callbackLoggedIn: Function, callbackNotLoggedIn: Function) {
+	console.log("Validates token");
+	this.storage.get('token').then((token) => {
+	    console.log(token);
+	    this.token = token;
+	    let serverAddress = this.httpService.serverAddress;
+	    this.http.get(serverAddress + '/getusername/', {}, {
+		Authorization: 'Token ' + token
+	    })
+		.then(request => {
+		    console.log(request.status);
+		    console.log(request.data);
+		    console.log(request.headers);
+		    this.storage.get('username') .then((username) => {
+			console.log("User: " + username);
+			this.username = username;
+		    }).catch(error => {
+			console.log("Could not store username to storage.");
+			this.username = 'joakim';
+		    });
+		    callbackLoggedIn();
+		})
+		.catch(error => {
+		    console.log(error.status);
+		    console.log(error.error);
+		    console.log(error.headers);
+		    callbackNotLoggedIn();
 		});
-	}
+	}).catch(error => {
+	    console.log(error);
+	    console.log("Could not get token from storage.");
+	    callbackNotLoggedIn();
+	});
+    }
 }
 
